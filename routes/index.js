427e52d5ad6b077398c26af74177c3b6a8df998b
quangcuong0808 <cuongtq_58@vnu.edu.vn>
2016-02-21 22:16:29 +0700

    'second'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Eblog' });
+});
+router.get('/about', function(req, res, next) {
+	res.render('about');
+});
+
+router.get('/work', function(req, res, next) {
+	res.render('work');
+});
+
+router.get('/contact', function(req, res, next) {
+	res.render('contact');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/about', function(req, res, next) {
+	res.render('about');
+});
+
+router.get('/work', function(req, res, next) {
+	res.render('work');
+});
+
+router.get('/contact', function(req, res, next) {
+	res.render('contact');
+});
+
+
 module.exports = router;
