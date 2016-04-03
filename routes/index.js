89c9de32596f65dc59cc491d94bb37535f0ee1e1
quangcuong0808 <cuongtq_58@vnu.edu.vn>
2016-04-04 00:08:12 +0700

    final fix for simpleApp

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var jade = require('jade');
 var router = express.Router();
 
+var num = 0;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { 
-		title: 'Eblog'
+		title: 'Eblog',
+		number: num
 	});
+	num = 0;
 });
 
 router.get('/about', function(req, res, next) {
@@ -21,20 +24,9 @@ router.get('/contact', function(req, res, next) {
 	res.render('contact');
 });
 
-router.get('/images/w2/*', function(req, res, next) {
-	var imagePath = req.url,
-	    url = 'https://eblogcdcn.herokuapp.com' + imagePath;
-
-	request(url, function(error, response, img) {
-		if(!error && response.statusCode === 200) {
-			res.header('Access-Control-Allow-Origin', '*');
-			res.writeHead(200, {'Content-Type': 'image/png' });
-			res.end(img, 'binary');
-	    } else if(response.statusCode === 404) {
-			res.status(404);
-			res.type('txt').send('oops');
-	    }
-	});
+router.get('/bt/:number', function(req, res, next) {
+	num = req.params.number;
+	res.redirect('/');
 });
 
 module.exports = router;
